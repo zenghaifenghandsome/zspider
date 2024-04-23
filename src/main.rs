d3d0371c5f7c2188c86e5a2f8202399af1df5f27
zenghaifeng <zenghaifenghandsom@163.com>
2024-04-24 00:56:28 +0800

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() -> Result<(),Box<dyn std::error::Error>>{
     println!("Hello, world!");
+    let resp = reqwest::get("https://www.baidu.com")
+        .await?
+        //.json::<HashMap<String,String>>()
+        .text()
+        .await?;
+    println!("{resp:?}");
+    Ok(())
 }
